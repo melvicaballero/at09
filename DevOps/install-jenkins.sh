bdea739c54c9526c60c72e6ff275fa5cf8ff5545
melvicaballero <melvi.caballero@gmail.com>
2019-06-17 20:10:50 -0400

    files

---
+++ DevOps/install-jenkins.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install openjdk-8-jdk -y
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install jenkins -y
